fa122cc1e11984d170c397fa18d7cf501eadecac
Stephan Hüwe <sh@huestel.de>
2021-12-22 15:04:01 +0100

    Splitting code to own file First Compiling version

---
+++ Daily_Blazor_App/Daily_Blazor_App/Pages/FetchData.razor.cs
@@ -0,0 +1,14 @@
+﻿using Daily_Blazor_App.Data;
+
+namespace Daily_Blazor_App.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
