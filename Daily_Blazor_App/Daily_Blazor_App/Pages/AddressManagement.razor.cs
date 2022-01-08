a6b067213dcc10d5dc45d57decf9d70957daacff
stephanhuewe <sh@huewe.info>
2022-01-08 10:36:58 +0100

    Extending AddressManager... Reverted everything yesterday... So one commit  lost :-( Try again.

--- Daily_Blazor_App/Daily_Blazor_App/Pages/AddressManagement.razor.cs
+++ Daily_Blazor_App/Daily_Blazor_App/Pages/AddressManagement.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace Daily_Blazor_App.Pages
+﻿using Daily_Blazor_App.Data;
+
+namespace Daily_Blazor_App.Pages
 {
     public partial class AddressManagement
     {
+        private Person[] forecasts;
+        private string newFirstName;
+        private string newLastName;
+        private string nameReverse;
+        private string searchText;
+        private bool visible;
+        private bool detailsVisbile;
+        private int rating;
+        private HashSet<Person> selectedItems = new HashSet<Person>();
+        private Person currentPerson;
+        bool _forceRerender;
     }
 }
